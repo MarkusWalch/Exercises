bc2ec0b762d450d0905e0d4a133e046af502d105
MarkusWindows <markus.walchhofer@gmx.at>
2022-03-19 10:16:46 +0700

    Weiter gehts mit dem TreeConstructor

--- javaScript/treeConstructor.js
+++ javaScript/treeConstructor.js
@@ -3,49 +3,133 @@
 function TreeConstructor(strArr) {
 
 
-    let a = new node(1);
-    let b = new node(5);
-    let c = new node(10);
+    let a = new BinaryNode(1);
+    let b = new BinaryNode(5);
+    let c = new BinaryNode(10);
+    let d = new BinaryNode(5);
+    let tree = new BinSearchTree();
 
+    tree.add(b.value);
+    tree.add(a.value);
+    tree.add(c.value);
 
+    tree.add(d.value);
+
+    console.log(tree);
     console.log();
-    //B ist der ChildNode von A
-    a.left = b;
-    console.log(a.left);
-    console.log(a);
-    console.log(b);
+    console.log();
+    console.log();
+    
+    
 
 
     return false;
 }
 
-class node {
+class BinSearchTree {
+    constructor() {
+        this.root = null;
+        this.size = 0;
+    }
+    add(value) {
+        if (this.root == null) {
+            this.root = new BinaryNode(value);
+            return;
+        }
+
+        if (this.find(value)) {
+            console.log("Der Wert " + value + " kommt doppelt vor, ich mache nichts!");
+            return;
+        }
+
+        let node = this.root;
+        while (this.nextNode(value, node)) {
+            node = this.nextNode(value, node);       
+        }
+        value < node.value ? node.left = new BinaryNode(value) : node.right = new BinaryNode(value);
+    }
+
+    find(value) {
+        if (this.root.value == value) {
+            return true;
+        }
+        let node = this.root;
+        while(this.nextNode(value, node)) {
+            if ((node = this.nextNode(value, node)) == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    remove(value) {}
+    getMax() {}
+    getMin() {}
+
+    nextNode(value, node) {
+        if (value < node.value) {
+            return node.left;
+        }
+        else {
+            return node.right;
+        }
+    }
+}
+class BinaryNode {    //Binary Tree
+    // Das # bedeutet private deklariert
+    // Private können nur am Anfang deklariert werden
+    #left;
+    #right;
+    #parent;
+
     constructor(value) {
         this.value = value;
-        this._left = null;
-        this._right = null;
-        this._parent = null;
+        this.#left = null;
+        this.#right = null;
+        this.#parent = null;
     }
 
     get left() {
-        return this._left;
+        return this.#left;
     }
+    //Note for me - name of variable and method should have different names
+    //Otherwise with this.left = node; you would make an infinite loop
     set left(node) {
-        this._left = node;
-        if (node) {
-            node.parent = this;
-          }
+        this.#left = node;
+//        if (node) {
+//            node.parent = this;
+//          }
     }
     get right() {
-        return this._right;
+        return this.#right;
     }
     set right(node) {
-        this._right = node;
-        if (node) {
-            node.parent = this;
-          }
+        this.#right = node;
+//        if (node) {
+//            node.parent = this;
+//          }
     }
-    
+
+    get parent() {
+        return this.#parent;
+    }
+
+    set parent(node) {
+        this.#parent = node;
+/*
+        if (node) {
+            if (node.left == null) {
+                node.left = this;
+            }
+            else if (node.right == null) {
+                node.right = this;
+            }
+            else {
+                console.log("Die Node " + node.value + " hat schon 2 children!");
+            }
+        }
+*/
+    }
 }
 
 // keep this function call here 
