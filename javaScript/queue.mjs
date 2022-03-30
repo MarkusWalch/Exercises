b2be4e4cd3fc760f47657260c4a1075d95b1fd5d
MarkusWindows <markus.walchhofer@gmx.at>
2022-03-30 12:30:10 +0700

    Did add a priority Queue with Arrays

--- javaScript/queue.mjs
+++ javaScript/queue.mjs
@@ -31,6 +31,42 @@ class Queue {
     }
 }
 
+class PriorityQueueArr extends Queue {
+    constructor() {
+        super();
+    }
+
+    enqueue(anyElement, intPriority) {
+        let item = {
+            element:anyElement, priority:intPriority
+        };
+
+        this.size = this.elements.push(item);
+    }
+    dequeue() {
+        let index = this.highestPriorityIndex();
+        //splice return an array, I found no other way to remove an element in the middle
+        let result = this.elements.splice(index, 1)[0];
+        result ? this.size-- : this.size = 0;
+        return result.element;
+    }
+    peek() {
+        return this.elements[this.highestPriorityIndex()].element;
+    }
+
+    highestPriorityIndex() {
+        let highestPriorityIndex = 0;
+        let highestPriority = 0;
+        for (let index in this.elements) {
+            if (this.elements[index].priority > highestPriority) {
+                highestPriority = this.elements[index].priority;
+                highestPriorityIndex = index;
+            }
+        }
+        return highestPriorityIndex;
+    }
+}
+
 function test () {
     let snake = new Queue();
     console.log(snake.isEmpty());
@@ -47,6 +83,21 @@ function test () {
     console.log(snake);
     console.log(snake.peek());
     console.log(snake.isEmpty());
+
+    //No overloading methods with parameters in js
+    console.log("Priority Queue");
+    let row = new PriorityQueueArr();
+    row.enqueue("Me", 10);
+    console.log(row.size);
+    row.enqueue("Her", 20);
+    row.enqueue("Him", 298);
+    row.enqueue("It", -31);
+    console.log(row);
+    console.log(row.dequeue());
+    console.log(row);
 }
 
+//test();
+
 export { Queue };
+export { PriorityQueueArr };
