e14d4465f3e9218f36b3c01ed33a60f884ede297
MarkusLinux <linux@markus.at>
2022-04-01 17:17:16 +0200

    git test

--- python/consume_api.py
+++ python/consume_api.py
@@ -8,6 +8,7 @@ import requests
 response = requests.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
 
 print(response.json()["items"])
+#Weiter gehts morgen :)
 
 #for data in response.json()["items"]:
 #    print(data["title"])
